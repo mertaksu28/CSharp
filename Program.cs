e521ed616009298a8f8c47b1e286e33bd6c6f4cc
Mert AKSU <65791804+mertaksu28@users.noreply.github.com>
2021-01-17 13:59:48 +0300

    Add files via upload
    Calısma2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekrar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 10;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 28;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------");
+            }
+            Console.WriteLine("------------METOTLAR------------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Elma", "Amasya Elması", 15, 10);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 28, 12);
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
